d07ff68dcf81712f9730171f64ecc3c5c982b60a
Himanshu Pandey <114969802+Himanshu20752005@users.noreply.github.com>
2024-10-09 23:46:56 +0530

    Create Technologies.jsx
    Technologies section of portfolio - basic

---
+++ Technologies.jsx
@@ -0,0 +1,24 @@
+import { RiReactjsLine } from "react-icons/ri"
+import { TbBrandNextjs } from "react-icons/tb"
+import { SiMongodb } from "react-icons/si"
+import { DiRedis } from "react-icons/di"
+import { FaNodeJs } from "react-icons/fa"
+import { BiLogoPostgresql } from "react-icons/bi" 
+
+const Technologies = () => {
+  return (
+    <div className=" border-b border-neutral-800 pb-24">
+      <h1 className="border-b text-center text-3xl mt-6 p-20">Technologies</h1>
+      <div className="flex flex-wrap items-center justify-center gap-4 py-9">
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+      <RiReactjsLine className="text-5xl text-cyan-400"/>
+        </div>
+
+      
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Technologies
