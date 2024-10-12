3080ecf05973398de25cf1fbd6fba47bd89e360c
Himanshu Pandey <114969802+Himanshu20752005@users.noreply.github.com>
2024-10-12 16:01:03 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import About from "./components/About"
 import Contact from "./components/Contact"
 import Experience from "./components/Experience"
 import Hero from "./components/Hero"
-import Navbar from "./components/navbar.jsx"
+import Navbar from "./components/navbar"
 import Projects from "./components/Projects"
 import Technologies from "./components/Technologies"
 function App() {
